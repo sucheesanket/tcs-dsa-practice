305ebd9f708fe92993e9b2cb38bf022d5dd8b292
sucheesanket <167225590+sucheesanket@users.noreply.github.com>
2026-07-10 17:56:05 +0530

    day-4 complete

---
+++ 10th July/maximumsubarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentsum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            currentsum=max(nums[i],currentsum+nums[i]);
+            maxsum=max(maxsum,currentsum);
+        }
+        return maxsum;
+        
+    }
+};
+//tcs practice 53
